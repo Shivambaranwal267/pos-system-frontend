813dbc0e0bc933f2f0b1fd15d29f39d6bc250324
Shivam Baranwal <shivambaranwal267@gmail.com>
2026-03-09 23:13:35 +0530

    feat: add DiscountSection component for applying percentage or fixed discounts.

--- src/pages/cashier/CustomerPaymentSection/DiscountSection.jsx
+++ src/pages/cashier/CustomerPaymentSection/DiscountSection.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { User } from "lucide-react";
+import { Tag } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 const DiscountSection = () => {
-  const [discount, setDiscount] = useState(0);
+  const [discount, setDiscount] = useState({ type: "percentage", value: 0 });
 
   const handleDiscountChange = (e) => {
     const value = e.target.value;
@@ -14,23 +14,25 @@ const DiscountSection = () => {
   return (
     <div className="p-4 border-b">
       <h2 className="mb-3 flex text-lg font-semibold items-center">
-        <User className="w-5 h-5 mr-2" /> Customer
+        <Tag className="w-5 h-5 mr-2" /> Discount
       </h2>
       <div className="space-y-3">
         <Input
           placeholder="Enter discount amount"
           type="number"
-          value={discount}
+          value={discount.value}
           onChange={handleDiscountChange}
         />
-        <div>
-          <Button className="flex-1"
+        <div className="flex gap-5">
+          <Button
+            className="flex-1"
             onClick={() => setDiscount({ ...discount, type: "percentage" })}
             variant={discount.type === "percentage" ? "default" : "outline"}
           >
             %
           </Button>
-          <Button className="flex-1"
+          <Button
+            className="flex-1"
             onClick={() => setDiscount({ ...discount, type: "fixed" })}
             variant={discount.type === "fixed" ? "default" : "outline"}
           >
